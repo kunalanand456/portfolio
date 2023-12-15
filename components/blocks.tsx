12f878ec9319232060cd64c4de753931a74148a6
kunalanand456 <56056910+kunalanand456@users.noreply.github.com>
2023-12-14 22:36:49 -0800

    Update blocks.tsx

--- components/blocks.tsx
+++ components/blocks.tsx
@@ -15,22 +15,12 @@ export default function Blocks() {
           <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
 
             {/* 1st item */}
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="100"data-aos-anchor="[data-aos-id-blocks]">
             <Link href="https://www.linkedin.com/in/kunal-a/" target = '_blank' className="flex justify-center items-center text-white-600 bg-black hover:text-gray-100 hover:bg-blue-800 border-4 border-white   rounded-full transition duration-150 ease-in-out" aria-label="Linkedin">
         <svg className="w-20 h-20 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
           <path d="M23.3 8H8.7c-.4 0-.7.3-.7.7v14.7c0 .3.3.6.7.6h14.7c.4 0 .7-.3.7-.7V8.7c-.1-.4-.4-.7-.8-.7zM12.7 21.6h-2.3V14h2.4v7.6h-.1zM11.6 13c-.8 0-1.4-.7-1.4-1.4 0-.8.6-1.4 1.4-1.4.8 0 1.4.6 1.4 1.4-.1.7-.7 1.4-1.4 1.4zm10 8.6h-2.4v-3.7c0-.9 0-2-1.2-2s-1.4 1-1.4 2v3.8h-2.4V14h2.3v1c.3-.6 1.1-1.2 2.2-1.2 2.4 0 2.8 1.6 2.8 3.6v4.2h.1z" />
         </svg>
       </Link>
-            </div>
-
-            {/* 2nd item */}
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks]">
-            <Link href="https://github.com/kunalanand456" target='_blank' className="flex justify-center items-center text-white-600 bg-black-800 border-4 border-white hover:text-gray-100 hover:bg-gray-600 rounded-full transition duration-150 ease-in-out" aria-label="Github">
-        <svg className="w-20 h-20 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-          <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
-        </svg>
-      </Link>
-             
             </div>
 
             {/* 3rd item */}
@@ -55,7 +45,7 @@ export default function Blocks() {
 
           
             
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="500" data-aos-anchor="[data-aos-id-blocks]">
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="400" data-aos-anchor="[data-aos-id-blocks]">
             <Link href="https://open.spotify.com/user/kunalanand456?si=EE8d0qM3RNSlNAZ9tS5xSA&nd=1&dlsi=b4f7f8b1e7b2406b" target = '_blank' className="flex justify-center items-center text-black-600 bg-green hover:text-gray-100 border-4 border-white hover:bg-green-400  rounded-full transition duration-150 ease-in-out" aria-label="Instagram">
             <svg className="w-20 h-20 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
   <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0 3c-3.866 0-7 3.134-7 7s3.134 7 7 7 7-3.134 7-7-3.134-7-7-7zm3.21 10.096c-.125.207-.394.271-.6.146-1.643-1.005-3.712-1.232-6.149-.675-.235.053-.469-.094-.522-.328-.054-.235.092-.469.328-.523 2.666-.609 4.954-.347 6.799.78.205.126.27.395.144.6zm.857-1.906c-.158.257-.494.338-.751.18-1.881-1.156-4.75-1.491-6.975-.816-.289.088-.594-.075-.681-.363-.087-.289.076-.593.364-.681 2.542-.771 5.703-.398 7.863.93.257.158.338.494.18.75zm.074-1.984c-2.257-1.34-5.979-1.464-8.133-.81-.345.105-.711-.09-.816-.436-.105-.346.09-.712.436-.817 2.473-.75 6.583-.605 9.181.937.311.184.413.586.229.897-.185.311-.587.413-.897.229z"/>
