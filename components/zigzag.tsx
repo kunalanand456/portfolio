746d7875552ae067ad9fe213eaa5b5e2469d645f
kunalanand456 <56056910+kunalanand456@users.noreply.github.com>
2023-12-24 17:26:17 -0800

    Update zigzag.tsx

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-import kunal from '@/public/images/kunalprofilepic.jpg'
+import kunal from '@/public/images/kunalcover.jpg'
 
 export default function Zigzag() {
   return (
@@ -22,7 +22,7 @@ export default function Zigzag() {
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
-                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={kunal} width={540} height={300} alt="Features 02" />
+                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={kunal} width={450} height={400} alt="Features 02" />
               </div>
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
